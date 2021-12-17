a16e2b0c5521c790b72793973be32f500cf2f725
handod <69374221+handod@users.noreply.github.com>
2021-12-17 23:08:57 +0200

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      :right="true"
+      temporary
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar fixed app>
+      <v-spacer />
+      <v-toolbar-title v-text="title" right="true"  />
+      <v-spacer />
+      <v-btn
+        icon
+        @click.stop="drawer = !drawer"
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer
+      :absolute="!fixed"
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      title: 'רוני סיידון - אינסטלטור מומחה'
+    }
+  }
+}
+</script>
+
+<style>
+.v-main__wrap {
+  /* background-color: #FFF8E1; */
+  background-color: #B39DDB;
+}
+</style>
